140733150d714e1ecde0551f4810a959573ae213
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-01-07 20:20:14 -0300

    refactor app front file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,8 @@ import axios from 'axios'
 import { useEffect } from 'react'
 
 function App() {
-  useEffect(() => {
-    const blablabla = axios.get('http://localhost:3000/setCookie', {
-      withCredentials: true
-    })
-    console.log(blablabla)
-  }, [])
-
+  useEffect(() => axios.get('http://localhost:3000/setCookie', { withCredentials: true }), [])
+  
   return <h1>Hello World!</h1>
 }
 
